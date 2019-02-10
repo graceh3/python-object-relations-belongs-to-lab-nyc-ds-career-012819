fa160caa97c0b26ab7febf78050354a6c62cc18a
graceh3 <41977636+graceh3@users.noreply.github.com>
2019-02-09 22:01:54 -0500

    answers

--- car.py
+++ car.py
@@ -8,3 +8,53 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+        
+    @classmethod
+    def add_car(cls, car_instance):
+        cls._all.append(car_instance)
+        return cls._all
+        
+    @property
+    def make(self):
+        return self._make
+    
+    @property
+    def model(self):
+        return self._model
+    
+    @property
+    def year(self):
+        return self._year
+    
+    @property
+    def owner(self):
+        return self._owner
+    
+    @make.setter
+    def make(self, new_make):
+        self._make = new_make
+        
+    @model.setter
+    def model(self, new_model):
+        self._model = new_model
+    
+    @year.setter
+    def year(self, new_year):
+        self._year = new_year
+        
+    @owner.setter
+    def owner(self, new_owner):
+        self._owner = new_owner
+    
+    @classmethod
+    def all(cls):
+        return cls._all
+    
+    @classmethod
+    def cars_driven_by(cls, occupation):
+        result = []
+        for car in Car._all:
+            if car.owner.occupation == occupation:
+                result.append(car.owner.name.split()[0].lower())
+        return result
+   
--- person.py
+++ person.py
@@ -3,6 +3,56 @@ from car import Car
 
 class Person:
 
+    _allp=[]
+    
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+        Person.add_person(self)
+        
+    @classmethod
+    def add_person(cls, person_instance):
+        cls._allp.append(person_instance)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def occupation(self):
+        return self._occupation
+    
+    @classmethod
+    def has_oldest_car(cls):
+        car_years = {}
+        for car in Car._all:
+            car_years.update({car.year:car.owner.name})
+        keys = sorted([key for key in car_years.keys()])
+        return car_years[keys[0]]
+
+    @classmethod
+    def drives_a(cls, make):
+        return[(car.owner.name).split()[0].lower() for car in Car._all if car.make == make]
+    
+    def drives_same_make_as_me(self):
+        make1 = ""
+        name1 = ""
+        result = []
+        for car in Car._all:
+            if car.owner.name == self._name: 
+                make1 = car.make
+        result.extend(self.drives_a(make1))
+        result.remove(self._name.split()[0].lower())
+        return result
+        
+    @classmethod
+    def all_persons(cls):
+        return cls._allp
+        
+        
+        
+        
+        
+        
+        
+            
